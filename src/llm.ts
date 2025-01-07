99bf1be13d73f7f790f5a7c7aec17be9cf673732
Diana <dianaalokhina@gmail.com>
2025-01-06 21:25:28 -0500

    clean code

--- src/llm.ts
+++ src/llm.ts
@@ -6,19 +6,5 @@ export const runLLM = async ({ userMessage }: { userMessage: string }) => {
     temperature: 0.1,
     messages: [{ role: 'user', content: userMessage }],
   })
-  console.log(JSON.stringify(response, null, 2));
   return response.choices[0].message.content
 }
-
-// const completion = await openai.chat.completions.create({
-//     model: "gpt-4o-mini",
-//     messages: [
-//         { role: "system", content: "You are a helpful assistant." },
-//         {
-//             role: "user",
-//             content: "Write a haiku about recursion in programming.",
-//         },
-//     ],
-// });
-
-// console.log(completion.choices[0].message);
